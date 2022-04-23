06df5547339c659678dddd1299651fff029f87c6
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-23 07:26:40 -0700

    21-printf-exercises

---
+++ S01-basics/19-printf/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		planet   = "venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	// swiss army knife %v verb
+	fmt.Printf("Planet: %v\n", planet)
+	fmt.Printf("Distance: %v millions kms\n", distance)
+	fmt.Printf("Orbital Period: %v days\n", orbital)
+	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+
+	// argument indexing - indexes start from 1
+	fmt.Printf(
+		"%v is %v away. Think! %[2]v kms! %[1]v OMG.\n",
+		planet, distance,
+	)
+}
