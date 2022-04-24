483d21f4494d738e0e2cd70296e921b15c9627a7
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-24 07:01:20 -0700

    39-err-handling-exercises

---
+++ 11-if/exercises/07-leap-year/main.go
@@ -0,0 +1,65 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Leap Year
+//
+//  Find out whether a given year is a leap year or not.
+//
+// EXPECTED OUTPUT
+//  go run main.go
+//    Give me a year number
+//
+//  go run main.go eighties
+//    "eighties" is not a valid year.
+//
+//  go run main.go 2018
+//    2018 is not a leap year.
+//
+//  go run main.go 2100
+//    2100 is not a leap year.
+//
+//  go run main.go 2019
+//    2019 is not a leap year.
+//
+//  go run main.go 2020
+//    2020 is a leap year.
+//
+//  go run main.go 2024
+//    2024 is a leap year.
+// ---------------------------------------------------------
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Give me a year number")
+		return
+	}
+
+	year, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Printf("%q is not a valid year.\n", os.Args[1])
+		return
+	}
+
+	if year%4 == 0 && (year%100 != 0 || year%400 == 0) {
+		fmt.Printf("%d is a leap year.\n", year)
+	} else {
+		fmt.Printf("%d is not a leap year.\n", year)
+	}
+}
+
+// Review the original source code here:
+// https://github.com/golang/go/blob/ad644d2e86bab85787879d41c2d2aebbd7c57db8/src/time/time.go#L1289
