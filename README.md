2e009fac6e5b4cfccef3984da92f5109802e320c
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-04-28 11:18:46 -0700

    99-slice-exercises

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Go Bootcamp: Master Golang with 1000+ Exercises and Projects
 
 https://www.udemy.com/course/learn-go-the-complete-bootcamp-course-golang
+
+## Packages
+
+PrettySlice
+
+```sh
+go get -u github.com/inancgumus/prettyslice
+```
---
+++ S05-slices/98-uniq-generator/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	max, _ := strconv.Atoi(os.Args[1])
+
+	var uniques []int
+
+loop:
+	for len(uniques) < max {
+		n := rand.Intn(max) + 1
+		fmt.Print(n, " ")
+
+		for _, u := range uniques {
+			if u == n {
+				continue loop
+			}
+		}
+
+		uniques = append(uniques, n)
+	}
+
+	fmt.Println("\n\nuniques:", uniques)
+
+	sort.Ints(uniques)
+	fmt.Println("\n\nsorted:", uniques)
+
+	nums := [5]int{5, 4, 3, 2, 1}
+	sort.Ints(nums[:])
+	fmt.Println("\n\nnums:", nums)
+}
